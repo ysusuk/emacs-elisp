ca19d2063ee09a100d02739cf99fbbadc973eac2
ysusuk <yurasusuk@gmail.com>
2015-03-13 09:53:43 +0100

    creates getter with lowercased name (should be uppercased in future)

--- generate-getter-setter.el
+++ generate-getter-setter.el
@@ -1,5 +1,16 @@
-(defun generate-getter-setter (member-name)
-  (interactive "sMember-name: ")
-  (insert (concat "public get" member-name "() {\n"))
-  (insert "}")
+(defun generate-getter ()
+  (interactive)
+  (setq name (member-name))
+  (end-of-line)
+  (insert (concat "\npublic get" name "() {\n"))
+  (insert (concat "return " name ";"))
+  (insert "\n}")
   (indent-region 1 (point-max)))
+
+(defun member-name ()
+  (interactive)
+  (setq name (thing-at-point 'word))
+  (message name))
+
+(add-hook 'java-mode 'generate-getter)
+
